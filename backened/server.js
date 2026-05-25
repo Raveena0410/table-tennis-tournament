5f0a9228bd20eea1d66eb5de505fa489cbd79899
Raveena0410 <raveena935499@gmail.com>
2026-05-25 16:37:11 +0530

    Added initial router

--- backened/server.js
+++ backened/server.js
@@ -8,6 +8,11 @@ const cors=require('cors')
 app.use(express.json())
 app.use(cors())
 connectdb()
+
+app.get("/", ()=> {
+    return "I am serving on port 3000"
+});
+
 app.use('/router',route)
 app.listen(port,()=>{
     console.log(`port is listen on ${port}`)
