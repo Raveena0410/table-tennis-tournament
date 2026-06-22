cb22c4a03c614f217404c547f76e29244e3089d6
Raveena0410 <raveena935499@gmail.com>
2026-06-22 17:07:49 +0530

    Update server.js

--- backened/server.js
+++ backened/server.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const express=require('express')
-const app=express()
-const port=3000
-const route=require('./route/route')
-const connectdb=require('./config/config')
-const cors=require('cors')
-app.use(express.json())
-app.use(cors())
+// require('dotenv').config()
+// const express=require('express')
+// const app=express()
+// const port=3000
+// const route=require('./route/route')
+// const connectdb=require('./config/config')
+// const cors=require('cors')
+// app.use(express.json())
+// app.use(cors())
 connectdb()
 
 app.get("/", ()=> {
